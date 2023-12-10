507860cbf9edc2fa2d292e8e31cb673661c9e33e
atis02 <guwancikramow@gmail.com>
2023-12-11 00:12:56 +0500

    rr

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/E-portal-Admin-Page/',
+  base: '/E-portal/',
   plugins: [react()],
 })
